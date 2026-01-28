4ee0199ffbe8703bc66516916fbb75556cc2cd63
pogoppo <5822076+pogoppo@users.noreply.github.com>
2026-01-29 04:19:40 +0900

    404ページのデザイン反映

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -13,9 +13,17 @@
 	<GlobalHeader />
 </div>
 <main class="error">
-	<h1>{m['error.page.title']()}</h1>
-	<p>{m['error.page.description']()}</p>
-	<p>
+	<div class="error-heading-container">
+		<h1 class="error-heading">
+			<span class="error-heading-code">404</span>
+			<span>Not Found</span>
+		</h1>
+		<picture class="error-heading-image">
+			<img src="/images/app/mog-404.webp" alt="" />
+		</picture>
+	</div>
+	<p class="error-description">{m['error.page.description']()}</p>
+	<p class="error-back-to-home">
 		<a href="/">
 			{m['common.back-to-home']()}<FontAwesomeIcon icon={faCircleChevronRight} />
 		</a>
@@ -23,9 +31,6 @@
 </main>
 
 <style>
-	:root:has(main.error) {
-		--scroll-free-play: 0px;
-	}
 	.float-header {
 		position: absolute;
 		z-index: 2;
@@ -40,14 +45,57 @@
 		position: absolute;
 		width: 100%;
 		height: 100lvh;
-		h1 {
-			font-size: 1.5rem;
+	}
+	.error-heading-container {
+		container-type: inline-size;
+		display: grid;
+		grid-template-areas: 'image heading';
+		grid-template-columns: 40% 1fr;
+		align-items: flex-end;
+		justify-content: center;
+		gap: 1rem;
+		width: 100%;
+		max-width: 320px;
+	}
+	.error-heading {
+		grid-area: heading;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 0.5rem;
+		aspect-ratio: 1;
+		width: 100%;
+		background-image: radial-gradient(
+			closest-side,
+			var(--color-bg-primary) 0%,
+			var(--color-bg-primary) 75%,
+			transparent 100%
+		);
+		color: color-mix(in srgb, var(--color-fg-primary) 80%, transparent 20%);
+		font-size: 6cqw;
+		line-height: 1;
+	}
+	.error-heading-code {
+		font-size: 18cqw;
+		font-weight: bold;
+	}
+	.error-heading-image {
+		grid-area: image;
+		display: flex;
+		> img {
+			aspect-ratio: 1;
+			width: 100%;
+			height: auto;
+			scale: 1.2;
 		}
-		p {
-			font-size: 0.9rem;
-		}
-		a {
-			color: var(--color-link);
-		}
+	}
+	.error-description {
+		margin-top: 1rem;
+		font-size: 0.9rem;
+	}
+	.error-back-to-home a {
+		color: var(--color-link);
+		font-size: 0.9rem;
 	}
 </style>
