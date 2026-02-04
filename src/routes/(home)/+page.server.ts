2196ad67b537bf6e0241ff8c1bd2a296aa52a02c
pogoppo <5822076+pogoppo@users.noreply.github.com>
2026-02-04 22:26:07 +0900

    コンテンツ調整

--- src/routes/(home)/+page.server.ts
+++ src/routes/(home)/+page.server.ts
@@ -2,8 +2,23 @@ import type { PageServerLoad } from './$types';
 import { groups } from '$lib/data/groups';
 import { articleRepository } from '$lib/repositories/article';
 
+const PRIORITIZED = ['_mock', 'tent', 'tarp', 'rope-work', 'moto-trouble'];
+
 export const load: PageServerLoad = () => {
 	const categories = articleRepository.getAllCategories();
+	categories.sort((a, b) => {
+		const aIndex = PRIORITIZED.indexOf(a);
+		const bIndex = PRIORITIZED.indexOf(b);
+		if (aIndex === -1 && bIndex === -1) {
+			return 0;
+		} else if (aIndex === -1) {
+			return 1;
+		} else if (bIndex === -1) {
+			return -1;
+		} else {
+			return aIndex - bIndex;
+		}
+	});
 
 	return {
 		groups,
